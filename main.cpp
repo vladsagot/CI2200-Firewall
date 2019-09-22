e6fcc2a55b2c7c5276bd28f349e0f5dad8aad294
vladimir <vladimir.sagot@ucr.ac.cr>
2019-09-22 15:35:46 -0600

    Avances

--- main.cpp
+++ main.cpp
@@ -60,6 +60,83 @@ vector<string> get_cookie_tokens(const string &line) {
     return lines_buffer;
 }
 
+void general_search_template() {
+    // ------------------------------------------------------------------
+    // Search Form
+    // ------------------------------------------------------------------
+    cout << "<h2>Search</h2>" << endl;
+    cout << "<form action=tarea1_seguridad?search-conn method='post'>" << endl;
+    cout << "<input type='text' name='name' size=10><br>" << endl;
+    cout << "<input type='submit' value='Search'>" << endl;
+    cout << "</form>" << endl;
+    cout << "<a href='../cgi-bin/tarea1_seguridad'>Go to the main page</a>" << endl;
+}
+
+void general_search_conn_template() {
+    // ------------------------------------------------------------------
+    // Search Database Connection
+    // ------------------------------------------------------------------
+    string line;
+    getline(cin, line);
+    vector<string> lines_buffer = get_tokens(line);
+
+    /*
+     * INPUTS:
+     * lines_buffer[0] : product_name
+     * */
+    string product_name = lines_buffer[0];
+
+    sql::mysql::MySQL_Driver *driver;
+    sql::Connection *con;
+    sql::Statement *stmt;
+    sql::ResultSet *res;
+
+    try {
+        driver = sql::mysql::get_mysql_driver_instance();
+        con = driver->connect(sql_db_url, sql_db_username, sql_db_password);
+
+        string query = "SELECT name, quantity, price FROM products WHERE name LIKE '%" + product_name +
+                       "%' ORDER BY name;";
+
+        stmt = con->createStatement();
+        stmt->execute("USE seguridad1");
+        res = stmt->executeQuery(query);
+
+        cout << "<table><tr> <th>Name</th> <th>Quantity</th> <th>Price</th> </tr>" << endl;
+        while (res->next()) {
+            cout << "<tr>" << endl;
+            cout << "<th>" << res->getString("Name") << "</th>" << endl;
+            cout << "<th>" << res->getString("Quantity") << "</th>" << endl;
+            cout << "<th>" << "$" << res->getString("Price") << "</th>" << endl;
+            cout << "</tr>" << endl;
+        }
+        cout << "</table><br>" << endl;
+
+        delete res;
+        delete stmt;
+        delete con;
+    } catch (sql::SQLException &e) {
+        cout << "SQLException: " << e.what() << "<br>" << endl;
+    }
+    cout << "<a href='../cgi-bin/tarea1_seguridad'>Go to the main page</a>" << endl;
+}
+
+void contact_template() {
+    cout << "<h2>Contact</h2>" << endl;
+    cout << "<form action=tarea1_seguridad?contact-conn method='post'>" << endl;
+    cout << "Name <input type='text' name='name' size=10><br>" << endl;
+    cout << "Email <input type='text' name='email' size=10><br>" << endl;
+    cout << "Message <input type='text' name='message' size=40><br>" << endl;
+    cout << "<input type='submit' value='Send'>" << endl;
+    cout << "</form>" << endl;
+    cout << "<a href='../cgi-bin/tarea1_seguridad'>Go to the main page</a>" << endl;
+}
+
+void contact_conn_template() {
+    cout << "<h2>Contact</h2>" << endl;
+    
+}
+
 void no_login_template(const string &data) {
     cout << "<!doctype html>" << endl;
     cout << "<html lang='en'>" << endl;
@@ -84,50 +161,6 @@ void no_login_template(const string &data) {
         cout << "<input type='submit' value='Login'>" << endl;
         cout << "</form><br>" << endl;
         cout << "<a href='../cgi-bin/tarea1_seguridad'>Go to the main page</a>" << endl;
-    } else if (data == "login-conn") {
-        // ------------------------------------------------------------------
-        // Login Database Connection
-        // ------------------------------------------------------------------
-        string line;
-        getline(cin, line);
-        vector<string> lines_buffer = get_tokens(line);
-
-        /*
-         * INPUTS:
-         * lines_buffer[0] : username
-         */
-        string username = lines_buffer[0];
-
-        sql::mysql::MySQL_Driver *driver;
-        sql::Connection *con;
-        sql::Statement *stmt;
-        sql::ResultSet *res;
-
-        try {
-            driver = sql::mysql::get_mysql_driver_instance();
-            con = driver->connect(sql_db_url, sql_db_username, sql_db_password);
-
-            string query = "SELECT username FROM users WHERE username = '" + username + "';";
-
-            stmt = con->createStatement();
-            stmt->execute("USE seguridad1");
-            res = stmt->executeQuery(query);
-
-            if (res->next()) {
-                cout << "Successful login<br>" << endl;
-            } else {
-                cout << "Incorrect user or password<br>" << endl;
-            }
-
-            cout << "<a href='../cgi-bin/tarea1_seguridad'>Go to the main page</a>" << endl;
-
-            delete res;
-            delete stmt;
-            delete con;
-        } catch (sql::SQLException &e) {
-            cout << "SQLException: " << e.what() << "<br>" << endl;
-        }
-
     } else if (data == "sign-up") {
         // ------------------------------------------------------------------
         // Sign Up Form
@@ -144,6 +177,8 @@ void no_login_template(const string &data) {
         // ------------------------------------------------------------------
         // Sign Up Database Connection
         // ------------------------------------------------------------------
+        cout << "<h2>Sign up</h2>" << endl;
+
         string line;
         getline(cin, line);
         vector<string> lines_buffer = get_tokens(line);
@@ -181,62 +216,13 @@ void no_login_template(const string &data) {
         }
         cout << "<a href='../cgi-bin/tarea1_seguridad'>Go to the main page</a>" << endl;
     } else if (data == "search") {
-        // ------------------------------------------------------------------
-        // Search Form
-        // ------------------------------------------------------------------
-        cout << "<h2>Search</h2>" << endl;
-        cout << "<form action=tarea1_seguridad?search-conn method='post'>" << endl;
-        cout << "<input type='text' name='name' size=10><br>" << endl;
-        cout << "<input type='submit' value='Search'>" << endl;
-        cout << "</form>" << endl;
-        cout << "<a href='../cgi-bin/tarea1_seguridad'>Go to the main page</a>" << endl;
+        general_search_template();
     } else if (data == "search-conn") {
-        // ------------------------------------------------------------------
-        // Search Database Connection
-        // ------------------------------------------------------------------
-        string line;
-        getline(cin, line);
-        vector<string> lines_buffer = get_tokens(line);
-
-        /*
-         * INPUTS:
-         * lines_buffer[0] : product_name
-         * */
-        string product_name = lines_buffer[0];
-
-        sql::mysql::MySQL_Driver *driver;
-        sql::Connection *con;
-        sql::Statement *stmt;
-        sql::ResultSet *res;
-
-        try {
-            driver = sql::mysql::get_mysql_driver_instance();
-            con = driver->connect(sql_db_url, sql_db_username, sql_db_password);
-
-            string query = "SELECT name, quantity, price FROM products WHERE name LIKE '%" + product_name +
-                           "%' ORDER BY name;";
-
-            stmt = con->createStatement();
-            stmt->execute("USE seguridad1");
-            res = stmt->executeQuery(query);
-
-            cout << "<table><tr> <th>Name</th> <th>Quantity</th> <th>Price</th> </tr>" << endl;
-            while (res->next()) {
-                cout << "<tr>" << endl;
-                cout << "<th>" << res->getString("Name") << "</th>" << endl;
-                cout << "<th>" << res->getString("Quantity") << "</th>" << endl;
-                cout << "<th>" << "$" << res->getString("Price") << "</th>" << endl;
-                cout << "</tr>" << endl;
-            }
-            cout << "</table><br>" << endl;
-
-            delete res;
-            delete stmt;
-            delete con;
-        } catch (sql::SQLException &e) {
-            cout << "SQLException: " << e.what() << "<br>" << endl;
-        }
-        cout << "<a href='../cgi-bin/tarea1_seguridad'>Go to the main page</a>" << endl;
+        general_search_conn_template();
+    } else if (data == "contact") {
+        contact_template();
+    } else if (data == "contact_conn") {
+        contact_conn_template();
     } else {
         // ------------------------------------------------------------------
         // Main Site
@@ -244,13 +230,29 @@ void no_login_template(const string &data) {
         cout << "<h2><a href='../cgi-bin/tarea1_seguridad?login'>Login</a></h2>" << endl;
         cout << "<h2><a href='../cgi-bin/tarea1_seguridad?sign-up'>Sign up</a></h2>" << endl;
         cout << "<h2><a href='../cgi-bin/tarea1_seguridad?search'>Search</a></h2>" << endl;
+        cout << "<h2><a href='../cgi-bin/tarea1_seguridad?contact'>Contact</a></h2>" << endl;
     }
-
     cout << "</body>" << endl;
     cout << "</html>" << endl;
 }
 
-void login_template(const string &data) {}
+void login_template(const string &data) {
+    cout << "<!doctype html>" << endl;
+    cout << "<html lang='en'>" << endl;
+
+    cout << "<head>" << endl;
+    cout << "<meta charset='utf-8'>" << endl;
+    cout << "<title>Store</title>" << endl;
+    cout << "<meta name='description' content='Store'>" << endl;
+    cout << "<meta name='author' content='Vladimir Sagot'>" << endl;
+    cout << "</head>" << endl;
+
+    cout << "<body>" << endl;
+    cout << "<h1><a href='../cgi-bin/tarea1_seguridad'>Store</a></h1>" << endl;
+    cout << "<h2><a href='../cgi-bin/tarea1_seguridad?login-check-out'>Log Out</a></h2>" << endl;
+    cout << "</body>" << endl;
+    cout << "</html>" << endl;
+}
 
 int main() {
     // ------------------------------------------------------------------
@@ -302,7 +304,6 @@ int main() {
 
             if (res->next()) {
                 cout << "Set-Cookie:UserID=" + res->getString("ID") + ";" << endl;
-                //cout << "Successful login<br>" << endl;
             } else {
                 cout << "Set-Cookie:UserID=;" << endl;
                 //cout << "Incorrect user or password<br>" << endl;
@@ -316,6 +317,10 @@ int main() {
         } catch (sql::SQLException &e) {
             //cout << "SQLException: " << e.what() << "<br>" << endl;
         }
+    }
+
+    if (data == "login-check-out") {
+        cout << "Set-Cookie:UserID=;" << endl;
     }
 
     // Verify is exist a logged user
@@ -334,6 +339,11 @@ int main() {
     }
 
     cout << "content-type: text/html" << endl << endl;
+
+    // Refresh page if an user is log in or log out
+    if (data == "login-check-in" || data == "login-check-out")
+        cout << "<meta http-equiv='refresh' content='0; url=../cgi-bin/tarea1_seguridad'>" << endl;
+
     cout << "DATA: " << data << "<br>" << endl;
     cout << "COOKIE: " << cookie << "<br>" << endl;
     if (user_login) {
